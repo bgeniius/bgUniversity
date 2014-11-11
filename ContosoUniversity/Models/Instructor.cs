879af247b9724bd777147beb1a01b211c9da9951
Bruno Cardoso <bitbruno@outlook.com>
2014-11-11 08:41:26 -0200

    Added Kendo DatePicker to all the date fields.

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -8,7 +8,6 @@ namespace BgeniiusUniversity.Models
     public class Instructor : Person
     {
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
 
